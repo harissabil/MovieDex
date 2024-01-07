13787d14e84f71fac2f43fbf88a4c9039c73936c
Haris <harissabil04@gmail.com>
2024-01-07 18:26:39 +0700

    Remove proguardFiles

--- feature/movie_favorite/build.gradle.kts
+++ feature/movie_favorite/build.gradle.kts
@@ -15,16 +15,6 @@ android {
             useSupportLibrary = true
         }
     }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
